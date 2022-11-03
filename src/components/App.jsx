596709771cd275681744421d81ad43cad3ee709d
Richard So <dev.richard.so@gmail.com>
2022-11-03 19:32:00 +1300

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+function App() {
+  return <h2>Hello, World!</h2>
+}
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { ReactDOM } from 'react'
+
+import App from './components/App'
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<App />, document.getElementById('app'))
+})
