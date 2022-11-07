6e8a5a6dedc927b88d8bcc6ff789045abe2f4fc0
Richard So <dev.richard.so@gmail.com>
2022-11-08 12:26:34 +1300

    ready

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const server = express()
+server.use(express.static(path.join(__dirname, 'public')))
 
 server.get('*', (req, res) => {
   res.sendFile(path.resolve('server/public/index.html'))
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function App() {
+  console.log('im here')
   return <h2>Hello, World!</h2>
 }
 export default App
